4df8ee0797ac4fa507848c5a0f7837249505d2ef
minabahadori <mbahahdori@potomacschool.org>
2021-10-13 21:49:42 -0400

    ComputerPlayer confusion

--- ComputerPlayer.py
+++ ComputerPlayer.py
@@ -1,1 +1,34 @@
+import random
 from Player import Player
+class ComputerPlayer (Player):
+
+
+    def __init__(self):
+        self.location
+        self.guess
+
+    def takeTurn(self):
+        x = random.randint(0, 10)
+        y = random.randint(0, 10)
+        changeRow(x)
+        changeCol(y)
+
+    def placeShip(self, ship, size):
+        xStart = random.randint(0, 10)
+        yStart = random.randint(0, 10)
+        direction = random.randint(0, 4)
+        # 0 = down, 1 = up, 2 = right, 3 = left
+        if direction == 0 :
+            if  (direction + size) < 10:
+                # add ship
+            else:
+                direction = 1
+        if direction == 1:
+            if  (direction - size) > -1:
+                #add ship
+            else:
+                direction = 0 # won't ever reach this
+        if direction == 2:
+            if (direciton + size) > 10:
+                # add ship
+
