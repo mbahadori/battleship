cd24ee3d8b7564be308a947d32086ae26f105e4b
ellieyoon <eyoon@potomacschool.org>
2021-10-13 21:49:44 -0400

    hi mina

--- HumanPlayer.py
+++ HumanPlayer.py
@@ -1,1 +1,24 @@
 from Player import Player
+
+class HumanPlayer(Player):
+
+    def__init__(self, location, guess):  #why is this not working
+        #Player.__init__(self)
+
+    location = []
+    guess = []
+    def__init__(location)
+    def__init__(guess)
+
+    def takeTurn(self):
+        inputA = int(input("Please enter a column number:"))
+        inputB = int(input("Please enter a row number:"))
+        stringDir= str(input("Please enter a direction:")) #hoz. or vertical
+        stringDir = str(input("Please enter a ship:"))
+        #changeRow and changeColumn
+
+
+    def placeShip(self, ship, size):
+
+    def isPlacementLegal(self, grid, row, col):
+
