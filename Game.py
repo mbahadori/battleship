357a83521010a2252b8b9cbc086a03641915eb0d
minabahadori <mbahahdori@potomacschool.org>
2021-10-21 09:28:56 -0400

    game class

--- Game.py
+++ Game.py
@@ -2,3 +2,7 @@ from HumanPlayer import HumanPlayer
 from ComputerPlayer import ComputerPlayer
 class Game:
 
+# StillHasShips false --> game is over
+
+    def __init__(self):
+        super().__init__()
