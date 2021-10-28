47925a2ccd73acfb6b90ee15a0fcab9ab191a911
minabahadori <mbahahdori@potomacschool.org>
2021-10-28 08:54:47 -0400

    cp game class

---
+++ CPGameClass.py
@@ -0,0 +1,24 @@
+from AdvancedComputerPlayer import AdvancedComputerPlayer
+from ComputerPlayer import ComputerPlayer
+
+# StillHasShips false --> game is over
+
+A1 = AdvancedComputerPlayer() #check if this works
+A1.createShipGrid()
+A1.printGrids()
+
+
+C1 = ComputerPlayer() #check if this works
+C1.createShipGrid()
+C1.printGrids()
+#print this out
+
+while A1.stillHasShips() == True or C1.stillHasShips() == True: #while player's ships are not sunk
+    A1.printGrids()
+    A1.takeTurn(C1)
+    if A1.stillHasShips() == False: # H1 has no more ships -- C1 won
+        print("Computer Player has won!")
+    C1.printGrids()
+    C1.takeTurn(A1)
+    if C1.stillHasShips() == False: # C1 has no more ships -- H1 won
+        print("Advanced Computer Player has won!")
